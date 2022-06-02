d763ff1860aa30dce98297a6b219927c1e460ee8
greenboy59 <81871149+greenboy59@users.noreply.github.com>
2022-06-02 18:25:08 +0200

    Initialisation du projet

---
+++ README.md
@@ -0,0 +1,39 @@
+# HotTakes
+
+## Pitch du projet
+Les sauces piquantes sont de plus en plus populaires, en grande partie grâce à la série YouTube « Hot Ones » . C’est pourquoi ce nouveau client, la marque de condiments à base de piment Piiquante, veut développer une application web de critique des sauces piquantes appelée « Hot Takes » . Piiquante a besoin d'un développeur back-end pour construire l'API.
+
+ <p align="center"><img width="276" alt="Capture d’écran 2022-06-02 à 15 16 12" src="https://user-images.githubusercontent.com/81871149/171637764-f5c2c0b1-dc8e-4d62-8aca-d9c137f649e1.png"></p>
+ 
+ <p align="center"><img width="553" alt="Capture d’écran 2022-06-02 à 15 18 44" src="https://user-images.githubusercontent.com/81871149/171638377-2bb47066-5225-435c-8ade-68581c99be83.png"></p>
+
+ <p align="center"><img width="540" alt="Capture d’écran 2022-06-02 à 15 19 03" src="https://user-images.githubusercontent.com/81871149/171638410-1b639e4f-68bf-435d-8dc0-bb604f0d2291.png"></p>
+
+Ce projet à été généré avec [Angular CLI](https://github.com/angular/angular-cli) version 13.2.4.
+
+# Lancer le projet
+
+## Clonage des repositories
+Backend: $ https://github.com/greenboy59/P6_julien_leclercq_openclassrooms.git  
+Frontend: $ https://github.com/OpenClassrooms-Student-Center/Web-Developer-P6.git 
+
+## Lancement du front
+$ cd ./frontend   
+$ npm install node-sass   
+$ npm run start   
+
+## Lancement du serveur Node
+$ cd ./backend  
+$ npm install   
+$ nodemon server   
+
+## Lancement de l'API
+$ Se connecter à l'url : http://localhost:4200
+
+## Technologies utilisées
+Node.JS / Express / MongoDB  
+Packages: Multer, JsonWebTokens, Helmet, cors, nodemon, mongoose-unique-validator, dotenv & Bcrypt
+
+## Aide supplémentaire
+
+Pour obtenir plus d'aide sur la CLI angular, utilisez `ng help` ou consultez la page [Angular CLI Overview and Command Reference] (https://angular.io/cli).
---
+++ backend/controllers/sauce.js
@@ -0,0 +1,122 @@
+const Sauce = require('../models/Sauce');
+const fs = require('fs');
+
+// Récupère l'intégralité des sauces
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => { res.status(200).json(sauces) })
+    .catch((error) => { res.status(400).json({ error: error }) });
+};
+
+// Récupère une sauce précise 
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      res.status(200).json(sauce);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
+};
+
+// Créer une nouvelle sauce
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// Modifie une sauce
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce), imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } :
+    {
+      ...req.body
+    };
+  // Modification de la sauce si vérif de l'id utilisateur et id sauce sont ok,sinon envoi d'une erreur code 403
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+    .catch(error => res.status(403).json({ error: error, message: 'Requête non autorisée' }));
+};
+
+// Supprime une sauce
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+// Like ou dislike une sauce
+exports.likeOrDislikeSauce = (req, res, next) => {
+  let sauceId = req.params.id
+  let userId = req.body.userId
+  let like = req.body.like
+  // Récupération de l'id de la sauce séléctionnée
+  Sauce.findOne({ _id: sauceId })
+    .then((sauce) => {
+      // Ajoute un like
+      if (like === 1 && !sauce.usersLiked.includes(userId)) {
+        Sauce.updateOne({ _id: sauceId },
+          {
+            // Incrémentation des likes et push dans tableau des usersLiked via opérateur mongoose $inc et $push
+            $inc: { likes: like++ },
+            $push: { usersLiked: userId },
+          })
+          .then(() => res.status(200).json({ message: 'Like ajouté !' }))
+          .catch(error => res.status(400).json({ error }))
+      }
+      // Ajoute un dislike
+      if (like === -1 && !sauce.usersDisliked.includes(userId)) {
+        Sauce.updateOne({ _id: sauceId },
+          {
+            $inc: { dislikes: like * -1 },
+            $push: { usersDisliked: userId },
+          })
+          .then(() => res.status(200).json({ message: 'Dislike ajouté !' }))
+          .catch(error => res.status(400).json({ error }))
+      }
+      // Enlève un like
+      if (like === 0 && sauce.usersLiked.includes(userId)) {
+        Sauce.updateOne({ _id: sauceId },
+          {
+            $inc: { likes: - 1 },
+            $pull: { usersLiked: userId },
+          }
+        )
+          .then(() => res.status(201).json({ message: 'Like annulé !' }))
+          .catch((error) => res.status(400).json({ error }));
+      }
+      // Enlève un dislike 
+      if (sauce.usersDisliked.includes(userId)) {
+        Sauce.updateOne({ _id: sauceId },
+          {
+            $inc: { dislikes: -1 },
+            $pull: { usersDisliked: userId },
+          }
+        )
+          .then(() => res.status(201).json({ message: 'Dislike annulé !' }))
+          .catch((error) => res.status(400).json({ error }));
+      }
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
