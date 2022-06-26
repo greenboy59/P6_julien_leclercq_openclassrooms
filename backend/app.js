a58cfd64c5d7ccf9011495e0c0f059a2336b6134
greenboy59 <81871149+greenboy59@users.noreply.github.com>
2022-06-26 14:32:05 +0200

    define base URL for axios. Begin the allposts view

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,12 @@ const cors = require('cors');
 // Lance express
 const app = express();
 
+// Middleware Helmet afin d'améliorer la sécurité. crossOriginResourcePolicy sur false afin d'autoriser l'affichage des images
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+})
+);
+
 // Importation des routes
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
@@ -19,12 +25,6 @@ dotenv.config();
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-// Middleware Helmet afin d'améliorer la sécurité. crossOriginResourcePolicy sur false afin d'autoriser l'affichage des images
-app.use(helmet({
-    crossOriginResourcePolicy: false,
-  })
-);
 
 // Gestion des CORS pour les connexions entre le back et le front
 app.use(cors())
